a1cb703bff94362de2af19cbc72114e68d7cebc2
Checho <checho9999@gmail.com>
2020-02-06 20:52:01 -0300

    Monitoreando lo que el usuario escribe en el input con useState

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Formulario = () => {
+
+    // Crear state de citas
+    const [ cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: '' 
+    });
+
+    // Extraer los valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+    // funcion que se ejecuta cada vez que en usuario escribe en un input
+    const actualizarState = (e) => {
+        console.log("Nombre: " + e.target.name + " Valor: " + e.target.value);
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    const submitCita = (e) => {
+        e.preventDefault(); // para que no envie los datos por el metodo GET con el querystring
+        alert('enviando...')
+    }
 
     return (       
 
@@ -8,7 +34,8 @@ const Formulario = () => {
 
             <h2>Crear Cita</h2>
 
-            <form      
+            <form
+                onSubmit={submitCita}            
             >
                 <label>Nombre Mascota</label>
                 <input
@@ -16,6 +43,8 @@ const Formulario = () => {
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre Mascota"
+                    onChange={actualizarState}
+                    value={mascota}
                 />
                 <label>Nombre Dueño</label>
                 <input
@@ -23,23 +52,31 @@ const Formulario = () => {
                     name="propietario"
                     className="u-full-width"
                     placeholder="Nombre Dueño de la Mascota"
+                    onChange={actualizarState}
+                    value={propietario}
                 />
                 <label>Fecha</label>
                 <input
                     type="date"
                     name="fecha"
-                    className="u-full-width"              
+                    className="u-full-width"
+                    onChange={actualizarState}              
+                    value={fecha}
                 />
                 <label>Hora</label>
                 <input
                     type="time"
                     name="hora"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value={hora}
                 />
                 <label>Sintomas</label>
                 <textarea
                     className="u-full-width"
                     name="sintomas"
+                    onChange={actualizarState}
+                    value={sintomas}
                 ></textarea>
                 <button
                     type="submit"
