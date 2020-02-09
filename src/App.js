bf012829b552a32f01deb7263c1ce4627a086fcb
Checho <checho9999@gmail.com>
2020-02-09 16:21:31 -0300

    Eliminado la cita; Mostrando un mensaje por pantalla por si hay o no citas

--- src/App.js
+++ src/App.js
@@ -14,6 +14,18 @@ function App() {
     ])
   }
 
+  //funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    //console.log(id);
+    const nuevasCitas = citas.filter(cita => cita.id !== id)
+  
+    guardarCitas(nuevasCitas)
+  
+  }
+  
+  //mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay Citas' : 'Administra tus Citas';
+
   return (
     <Fragment> 
       <h1>Administrador de Pacientes</h1>
@@ -26,12 +38,13 @@ function App() {
             />
           </div>
           <div className="one-half column">       
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {
               citas.map(cita => (                
                 <Cita 
                   key={cita.id}
                   cita={cita}
+                  eliminarCita={eliminarCita}
                 />
               ))
             }
--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Cita = ( {cita} ) => (
+const Cita = ( { cita, eliminarCita } ) => (
     <div className="cita">
         <p>Mascota: <span>{cita.mascota}</span></p>
         <p>Mascota: <span>{cita.propietario}</span></p>          
         <p>Mascota: <span>{cita.fecha}</span></p>          
         <p>Mascota: <span>{cita.hora}</span></p>                      
-        <p>Mascota: <span>{cita.sintomas}</span></p>                                                  
+        <p>Mascota: <span>{cita.sintomas}</span></p>
+
+        <button className="button-eliminar u-full-width"
+            onClick={ () => eliminarCita(cita.id) }
+        >Eliminar &times;</button>                                                        
     </div>
 );
 
