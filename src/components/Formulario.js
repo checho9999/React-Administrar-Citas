dfc6f0a5ceae6970a99df36bfbd0261d6b60caf4
Checho <checho9999@gmail.com>
2020-02-10 13:28:44 -0300

    Agregando las PropTypes de las props que llegan a los componentes Cita y Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import uuid from 'uuid/v4';
+import PropTypes from 'prop-types';
 
 const Formulario = ( {crearCita} ) => {
 
@@ -120,6 +121,10 @@ const Formulario = ( {crearCita} ) => {
             
         </Fragment>
      );
+}
+
+Formulario.propTypes = {
+    crearCita: PropTypes.func.isRequired
 }
 
 export default Formulario;
